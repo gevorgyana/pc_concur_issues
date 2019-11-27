516f282cd428d97495331cfa029fa7982343365a
Artyom Gevorgyan <artemhevorhian@gmail.com>
2019-11-27 15:55:41 +0200

    add logging

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
 /**
  * Simulate producer-consumer item loss that leads to
  * deadlock; try to increase the probability of
@@ -20,94 +21,109 @@
  * 2) check successful? yield
  * 3) sleep
  *
- * it is possible to run he pogram in
  * deadlock verification mode:
- * this is not enough, but still must hold
- * when a deadlock is present
- * do the following; when a thread enters critical region,
- * mark that we are now using the resource (buffer as a whole);
- * when we exit, we remove the flag;
- * when the other process exits, they too print the flag;
- * sumup:
- * every process does a print satement; when they hang, we see it (? it it okay?)
- * for formal verification we need a third party, observer()
- * observer will look and see the flags and watch out for
- * a case when a cycle appears;
-
- * example:
- * A sets request info - to a special store
- * B sets request info - to a special store
- * observer runs periodically and checks buffers,
- * when it sees the sign of deadlock, it reports
- * "DEADLOCK HAS BEEN ENCOUNTERED"
-
- * bullshi ^
- *         \
-
- * Чтобы проверить, есть ли делок, достаточно в этой задаче
+ * Чтобы проверить, есть ли делок, достаточно
  * проверить условие: (проодюсер хранит  count N, consumer - count 0)
- *
  *
  * TODO use thread-safe circular buffer? is it good in this case?
  * TODO implement demonstrational test;
- * TODO benchmark this code; come up with a python script to
+ * TODO benchmark this code; a python script to
  * automate this process and plot average values;
  */
 
-// Идейно мы расслоили присваивания на 2 слоя - глобальный и специфичный для
-// потока
-
 #define N 10
 int count = 0;
+
+/**
+   try this and examine compiler messages
+std::vector<std::thread> = {std::thread(***)}
+ */
 
 class PCDeadlockSimulator {
  public:
   void Run()
   {
 
+    std::vector <std::thread> threads(3);
+    threads[0] = std::thread(&PCDeadlockSimulator::Produce, this);
+    threads[1] = std::thread(&PCDeadlockSimulator::Consume, this);
+    threads[2] = std::thread(&PCDeadlockSimulator::Observe, this);
+
+    for (auto& thread: threads)
+    {
+      thread.join();
+    }
   }
 
  private:
+  std::condition_variable condvar_;
+  std::mutex mutex_;
+  std::atomic_bool flag_;
+  int producer_snapshot_counter_value_;
+  int consumer_snapshot_counter_value_;
+  int global_counter_value_;
+
   void Produce()
   {
+    spdlog::info("producer is running");
     while (true)
     {
-      producer_snapshot_counter_value_ = global_counter_value;
+
+
+      producer_snapshot_counter_value_ = global_counter_value_;
       if (producer_snapshot_counter_value_ == N)
       {
-        // cv.wait()
+        std::unique_lock<std::mutex> l(mutex_);
+        condvar_.wait(l);
       }
-      ++global_counter_value;
-      producer_snapshot_counter_value = global_counter_value;
-      if (producer_snapshow_counter)
+      ++global_counter_value_;
+      producer_snapshot_counter_value_ = global_counter_value_;
+      if (producer_snapshot_counter_value_)
+      {
+        condvar_.notify_one();
+      }
     }
   }
 
   void Consume()
   {
-
+    while (true)
+    {
+      consumer_snapshot_counter_value_ = global_counter_value_;
+      if (consumer_snapshot_counter_value_ == 0)
+      {
+        std::unique_lock<std::mutex> l(mutex_);
+        condvar_.wait(l);
+      }
+      --global_counter_value_;
+      consumer_snapshot_counter_value_ = global_counter_value_;
+      if (consumer_snapshot_counter_value_ == N - 1)
+      {
+        condvar_.notify_one();
+      }
+    }
   }
 
   void Observe()
   {
     while (true)
     {
-      if (producer_current_value_ == N && consumer_current_value_ == 0)
+      spdlog::info("observer is running");
+
+      if (producer_snapshot_counter_value_ == N
+          &&
+          consumer_snapshot_counter_value_ == 0)
       {
         std::cout << "Deadlock detected. Shutting down..." << std::endl;
       }
     }
   }
-
-  int producer_snapshot_counter_value_;
-  int consumer_snapshot_counter_value_;
 };
 
 void tst1_app_runs() {
   PCDeadlockSimulator simulator;
-
 }
 
-int main() {
-
+int main()
+{
 }
